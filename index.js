98d74a4a236388c73e1bbeefe060b2f38bd065ba
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-02-20 14:02:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+let peopleNames = ["Guadalupe", "Ollie", "Aki"];
+let giftType = "surprise";
+function writeCards(nameList, occasion) {
+  let messages = [];
+  for (let i = 0; i < nameList.length; i++) {
+    messages.push(
+      `Thank you, ${nameList[i]}, for the wonderful ${occasion} gift!`
+    );
+  }
+  return messages;
+}
+
+let birthdayCards = writeCards(peopleNames, giftType);
+
+console.log(birthdayCards);
+
+// While Loop
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num--);
+  }
+}
+
+countDown(10);
